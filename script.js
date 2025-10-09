a7f36e6c75845a4a3371dd01207b8058b8ff8159
lajelks <lajelks@yahoo.com>
2025-10-09 13:24:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,8 @@ function checkAnswer() {
   if (input === correctAnswer) {
     riddleBox.style.display = "none";
     postcard.style.display = "block";
-    // Show front, hide back initially
-    document.querySelector(".card .front").style.display = "block";
-    document.querySelector(".card .back").style.display = "none";
+    // Make sure the card is not flipped initially
+    document.querySelector(".card").classList.remove("flipped");
   } else {
     alert("Try again!");
   }
@@ -17,23 +16,9 @@ function checkAnswer() {
 
 document.addEventListener("DOMContentLoaded", function () {
   const card = document.querySelector(".card");
-  const cardFront = document.querySelector(".card .front");
-  const cardBack = document.querySelector(".card .back");
-
-  if (cardFront && cardBack) {
-    cardFront.style.display = "block";
-    cardBack.style.display = "none";
-
-    cardFront.addEventListener("click", function () {
-      cardFront.style.display = "none";
-      cardBack.style.display = "block";
-      card.classList.toggle("flipped", true);
-    });
-
-    cardBack.addEventListener("click", function () {
-      cardBack.style.display = "none";
-      cardFront.style.display = "block";
-      card.classList.toggle("flipped", false);
+  if (card) {
+    card.addEventListener("click", function () {
+      card.classList.toggle("flipped");
     });
   }
 });
